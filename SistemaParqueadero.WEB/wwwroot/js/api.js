4cada36deb2cf5b29a0ecd7a1c16c8a71449458a
Patricio Quiguango <patricioquiguango.08@gmail.com>
2026-02-22 23:45:04 -0500

    redirects

--- SistemaParqueadero.WEB/wwwroot/js/api.js
+++ SistemaParqueadero.WEB/wwwroot/js/api.js
@@ -1,11 +1,4 @@
-﻿const API_LOCAL_HTTPS = "https://localhost:7187";
-const API_LOCAL_HTTP = "http://localhost:5091";
-const API_PRODUCTION = "https://sistemaparqueadero-api.onrender.com";
-
-export const API_BASE =
-    (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-        ? (location.protocol === "https:" ? API_LOCAL_HTTPS : API_LOCAL_HTTP)
-        : API_PRODUCTION;
+﻿export const API_BASE = "https://sistemaparqueadero-api.onrender.com";
 
 export async function apiFetch(path, options = {}) {
     const token = localStorage.getItem("jwt") || sessionStorage.getItem("jwt");
